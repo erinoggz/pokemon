60308c826e0dabbddf07e776a1bf5836dbffb4d0
erin deji <erin.deji@gmail.com>
2024-04-12 21:16:10 +0100

    card image styling

--- app/component/card/List.tsx
+++ app/component/card/List.tsx
@@ -3,15 +3,12 @@ import {View, Text} from 'react-native';
 import Icon from 'react-native-vector-icons/Entypo';
 import styles from './styles';
 
-interface ListItemProps {
-  data: {name: string};
-}
-
-const List = ({data}: ListItemProps): React.JSX.Element => {
+const List = ({data}): React.JSX.Element => {
   return (
     <View style={styles.item}>
-      <View>
-        <Text>{data.name}</Text>
+      <View style={styles.itemLeft}>
+        <View style={styles.circleCard}></View>
+        <Text style={styles.text}>{data.name}</Text>
       </View>
       <Icon name="chevron-small-right" size={23} color="grey" />
     </View>
--- app/component/card/styles.ts
+++ app/component/card/styles.ts
@@ -4,12 +4,26 @@ import {COLORS, SIZES} from '../../../constants/theme';
 const styles = StyleSheet.create({
   item: {
     backgroundColor: COLORS.white,
-    height: 60,
+    height: 90,
     paddingHorizontal: SIZES.f1,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
     marginTop: SIZES.s1,
+    padding: 15,
+  },
+  itemLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  text: {fontSize: SIZES.f4},
+  circleCard: {
+    width: 80,
+    height: 80,
+    backgroundColor: COLORS.black,
+    borderRadius: 50,
+    marginRight: 15,
   },
 });
 
