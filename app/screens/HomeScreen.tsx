5c9541c6e8464879039ec0f1e99dd85adbc58e79
erin deji <erin.deji@gmail.com>
2024-04-12 15:28:01 +0100

    add search bar in home screen

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
+import Input from '../component/Input/Input';
+import Search from 'react-native-vector-icons/AntDesign';
 
 const HomeScreen = (): React.JSX.Element => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={{flex: 1}}>
+      <View>
+        <Input
+          background={'white'}
+          placeholder="Search for Pokemon..."
+          style={{paddingHorizontal: 50}}
+        />
+        <View style={{position: 'absolute', top: 18, left: 15}}>
+          <Search name="search1" size={25} color="grey" />
+        </View>
+      </View>
     </View>
   );
 };
