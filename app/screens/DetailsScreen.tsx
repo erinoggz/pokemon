1ffdb4eee800036fbf02a9082ae129df9d23247f
erin deji <erin.deji@gmail.com>
2024-04-13 20:47:54 +0100

    enhance details screen

--- app/screens/DetailsScreen.tsx
+++ app/screens/DetailsScreen.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {View, Text, Image, ScrollView, StyleSheet} from 'react-native';
 import {useQuery} from '@tanstack/react-query';
 import {fetchFn, searchPokemon} from '../api/api';
-import {getTypeColor, removeEscapeCharacters} from '../../constants/helper';
-import {COLORS} from '../../constants/theme';
+import {filterString, removeEscapeCharacters} from '../../constants/helper';
+import {COLORS, FONTS} from '../../constants/theme';
 
 export function DetailsScreen({route}) {
   const {name} = route.params;
@@ -15,7 +15,7 @@ export function DetailsScreen({route}) {
       enabled: !!data,
     },
   );
-  if (!data) return null;
+
   return (
     <ScrollView style={styles.view1}>
       <View style={styles.view2}>
@@ -32,10 +32,10 @@ export function DetailsScreen({route}) {
         </View>
       </View>
       <View style={styles.view3}>
-        <View>
-          <Text>Description</Text>
+        <View style={styles.container}>
+          <Text style={styles.title}>Description</Text>
           {!!species && (
-            <Text style={{fontSize: 12, color: 'blue'}}>
+            <Text style={styles.description}>
               {removeEscapeCharacters(
                 species.flavor_text_entries[0].flavor_text,
               )}
@@ -43,61 +43,52 @@ export function DetailsScreen({route}) {
           )}
         </View>
 
-        <View>
-          <Text>Types</Text>
-          <View style={{flexDirection: 'row'}}>
+        <View style={styles.container}>
+          <Text style={styles.title}>Types</Text>
+          <View style={styles.typesWrapper}>
             {data.types.map(type => (
-              <View
-                key={type.type.name}
-                style={{
-                  backgroundColor: `${getTypeColor(type.type.name)}`,
-                  padding: 4,
-                  alignItems: 'center',
-                  marginHorizontal: 4,
-                }}>
-                <Text
-                  style={{
-                    fontSize: 12,
-                    fontWeight: 'bold',
-                    textTransform: 'capitalize',
-                  }}>
-                  {type.type.name}
-                </Text>
+              <View key={type.type.name} style={styles.types}>
+                <Text style={styles.text2}>{type.type.name}</Text>
               </View>
             ))}
           </View>
         </View>
-        <View>
-          <Text>Weight</Text>
-          <Text>{data?.weight}</Text>
-        </View>
-        <View>
-          <Text>Abilities</Text>
-          {data?.abilities.map(data => (
-            <View key={data.ability.name}>
-              <Text
-                style={{
-                  fontSize: 12,
-                  textTransform: 'capitalize',
-                }}>
-                {data.ability.name}
-              </Text>
-            </View>
-          ))}
-        </View>
-        <View>
-          <Text>Stats</Text>
+
+        <View style={styles.container}>
+          <Text style={styles.title}>Stats</Text>
           {data?.stats.map(data => (
-            <View key={data.stat.name}>
-              <Text
-                style={{
-                  fontSize: 12,
-                  textTransform: 'capitalize',
-                }}>
-                {data.stat.name}: {data.base_stat}
-              </Text>
+            <View key={data.stat.name} style={styles.barContainer}>
+              <View>
+                <Text style={styles.statsText}>
+                  {filterString(data.stat.name)}
+                </Text>
+              </View>
+              <View style={styles.progressBar}>
+                <View
+                  style={[
+                    styles.progressBarInner,
+                    {width: `${data.base_stat}%`},
+                  ]}>
+                  <Text style={styles.baseStats}>{data.base_stat}%</Text>
+                </View>
+              </View>
             </View>
           ))}
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.title}>Weight</Text>
+          <Text style={styles.description}>{data?.weight}</Text>
+        </View>
+        <View style={styles.container}>
+          <Text style={styles.title}>Abilities</Text>
+
+          <View style={styles.typesWrapper}>
+            {data.abilities.map(type => (
+              <View key={type.ability.name} style={styles.types}>
+                <Text style={styles.text2}>{type.ability.name}</Text>
+              </View>
+            ))}
+          </View>
         </View>
       </View>
     </ScrollView>
@@ -115,41 +106,85 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-end',
   },
-  inputViews: {
-    paddingBottom: 16,
-  },
-  iconStyle: {
-    position: 'absolute',
-    top: 13,
-    right: 15,
-  },
-  imageIconStyle: {
-    position: 'absolute',
-    bottom: 15,
-    right: 2,
-  },
   view3: {
     flex: 3,
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-    backgroundColor: COLORS.white,
-    padding: 20,
-    marginTop: 54,
+    borderTopLeftRadius: 35,
+    borderTopRightRadius: 35,
+    backgroundColor: COLORS.primary,
+    padding: 30,
+    marginTop: 20,
   },
-  paddingView: {paddingHorizontal: 21, marginBottom: 23},
+  container: {
+    marginBottom: 15,
+  },
+  title: {
+    ...FONTS.base,
+    textTransform: 'uppercase',
+    marginBottom: 5,
+    color: COLORS.secondary,
+  },
+  description: {
+    ...FONTS.base_small,
+  },
+  text2: {
+    ...FONTS.small,
+    color: COLORS.white,
+    textTransform: 'uppercase',
+  },
+  typesWrapper: {flexDirection: 'row'},
+  types: {
+    backgroundColor: COLORS.secondary,
+    borderRadius: 25,
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    alignItems: 'center',
+    marginHorizontal: 4,
+  },
+  typesText: {
+    ...FONTS.base_small,
+    textTransform: 'capitalize',
+  },
+  statsText: {
+    ...FONTS.base_small,
+    textTransform: 'capitalize',
+    width: 50,
+  },
+  baseStats: {
+    ...FONTS.small,
+    paddingLeft: 5,
+    color: COLORS.white,
+  },
   image: {
-    width: 200,
-    height: 200,
-    borderRadius: 100,
-    borderColor: 'white',
+    width: 250,
+    height: 250,
+    borderRadius: 999,
+    borderColor: COLORS.white,
     borderWidth: 2,
   },
   text1: {
-    fontFamily: 'Raleway',
+    ...FONTS.large,
     textTransform: 'capitalize',
-
-    fontSize: 24,
     color: COLORS.white,
     marginTop: 18,
+  },
+  barContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: '80%',
+  },
+  progressBar: {
+    backgroundColor: COLORS.grey,
+    height: 15,
+    borderRadius: 12,
+    marginBottom: 7,
+    marginLeft: 10,
+    width: '100%',
+  },
+  progressBarInner: {
+    backgroundColor: COLORS.secondary,
+    height: 16,
+    borderRadius: 12,
+    width: '30%',
+    textAlign: 'center',
   },
 });
