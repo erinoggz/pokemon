d1121e7423fa26efe87b3ecd79dcb22331b84a16
erin deji <erin.deji@gmail.com>
2024-04-13 07:37:59 +0100

    add imageUri to list card

--- app/component/card/List.tsx
+++ app/component/card/List.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import Icon from 'react-native-vector-icons/Entypo';
 import styles from './styles';
 
-const List = ({data}): React.JSX.Element => {
+interface ListProps {
+  name: string;
+  background: string;
+  imageUri: string;
+}
+
+/**
+ * List component representing a Pokemon card.
+ * @param {ListProps} props - Component props.
+ * @returns {JSX.Element} - Rendered component.
+ */
+const List = ({name, background, imageUri}: ListProps): React.JSX.Element => {
   return (
     <View style={styles.item}>
       <View style={styles.itemLeft}>
-        <View style={styles.circleCard}></View>
-        <Text style={styles.text}>{data.name}</Text>
+        <View
+          style={[
+            styles.circleCard,
+            // Set background color based on Pokemon type
+            {backgroundColor: `${background}`},
+          ]}>
+          <Image
+            source={{
+              uri: imageUri,
+            }}
+            style={styles.image}
+          />
+        </View>
+        <Text style={styles.text}>{name}</Text>
       </View>
       <Icon name="chevron-small-right" size={23} color="grey" />
     </View>
--- app/component/card/styles.ts
+++ app/component/card/styles.ts
@@ -2,8 +2,16 @@ import {StyleSheet} from 'react-native';
 import {COLORS, SIZES} from '../../../constants/theme';
 
 const styles = StyleSheet.create({
+  circleCard: {
+    width: 80,
+    height: 80,
+    backgroundColor: COLORS.black,
+    borderRadius: 100,
+    marginRight: 15,
+    alignItems: 'center',
+  },
   item: {
-    backgroundColor: COLORS.white,
+    backgroundColor: COLORS.primary,
     height: 90,
     paddingHorizontal: SIZES.f1,
     flexDirection: 'row',
@@ -17,14 +25,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexWrap: 'wrap',
   },
+  image: {aspectRatio: 1, width: '80%', height: '80%'},
   text: {fontSize: SIZES.f4},
-  circleCard: {
-    width: 80,
-    height: 80,
-    backgroundColor: COLORS.black,
-    borderRadius: 50,
-    marginRight: 15,
-  },
 });
 
 export default styles;
