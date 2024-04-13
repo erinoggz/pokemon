ad0c3ce0189226e010d21cc79ee9697a9e8929e5
erin deji <erin.deji@gmail.com>
2024-04-13 22:15:32 +0100

    update styles for textinput

--- app/component/Input/style.ts
+++ app/component/Input/style.ts
@@ -1,11 +1,11 @@
 import {StyleSheet} from 'react-native';
-import {COLORS, SIZES} from '../../../constants/theme';
+import {COLORS} from '../../../constants/theme';
 
 const styles = StyleSheet.create({
   textinput: {
     borderColor: COLORS.grey,
-    fontSize: SIZES.f4,
     height: 60,
+    paddingHorizontal: 50,
   },
 });
 
